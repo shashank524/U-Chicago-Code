eb2d1f4dbcc3ad9593f94881edf05c398b2b07b9
shashank524 <saishashank524@gmail.com>
2023-04-13 19:38:55 -0400

    case 2

--- xchange-v1.0.2-115-g6b94+8ef0/clients/case2_draft.py
+++ xchange-v1.0.2-115-g6b94+8ef0/clients/case2_draft.py
@@ -13,6 +13,9 @@ import betterproto
 from math import sqrt, exp
 from scipy.stats import norm
 import os
+import re
+from math import log
+
 
 PARAM_FILE = "params.json"
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -48,27 +51,45 @@ class BlackScholesBot(UTCBot):
 
     async def handle_market_snapshot(self, snapshot: pb.MarketSnapshotMessage):
         for asset_code, book in snapshot.books.items():
-            option_type = asset_code[-1]  # Extract the option type (C or P)
-            strike = int(asset_code[-4:-1])  # Extract the strike price
+            print("Asset Code:", asset_code)
 
-            # Find the row with the closest underlying price to the current price
-            underlying_rows = self.option_data.iloc[(self.option_data['underlying'] - book.underlying_px).abs().argsort()[:1]]
-            underlying_price = underlying_rows.iloc[0]
+            # Extract the underlying price, strike price, and option type from the asset_code
+            match = re.match(r"(\d+(?:\.\d+)?)-(\d+(?:\.\d+)?)([CP])", asset_code)
 
-            if option_type == "C":
-                option_price = underlying_price[f"call{strike}"]
-            elif option_type == "P":
-                option_price = underlying_price[f"put{strike}"]
+            if match:
+                print("here")
+                underlying_px, strike_price, option_type = match.groups()
+                underlying_px = float(underlying_px)
+                strike_price = float(strike_price)
 
-            
-            if book.bids and book.asks:
-                bid_price = float(book.bids[0].px)
-                ask_price = float(book.asks[0].px)
-                
-                if option_price < bid_price:
-                    await self.place_order(asset_code, pb.OrderSpecType.LIMIT, pb.OrderSpecSide.BUY, self.params["qty"], option_price)
-                elif option_price > ask_price:
-                    await self.place_order(asset_code, pb.OrderSpecType.LIMIT, pb.OrderSpecSide.SELL, self.params["qty"], option_price)
+                # Get the closest underlying price row in the option data
+                underlying_rows = self.option_data.iloc[(self.option_data['underlying'] - underlying_px).abs().argsort()[:1]]
+                underlying_price = underlying_rows.iloc[0]
+
+                # Calculate the option price using the Black-Scholes model
+                bs_params = self.params.copy()
+                bs_params["K"] = strike_price
+                option_price = self.black_scholes(underlying_price, bs_params)
+
+                if option_type == "C":
+                    option_price = underlying_price[f"call{strike_price}"]
+                elif option_type == "P":
+                    option_price = underlying_price[f"put{strike_price}"]
+
+                if book.bids and book.asks:
+                    bid_price = float(book.bids[0].px)
+                    ask_price = float(book.asks[0].px)
+
+                    if option_price < bid_price:
+                        print("placing order")
+                        await self.place_order(asset_code, pb.OrderSpecType.LIMIT, pb.OrderSpecSide.BUY, self.params["qty"], option_price)
+                        print("placed order")
+                    elif option_price > ask_price:
+                        print("placing order")
+                        await self.place_order(asset_code, pb.OrderSpecType.LIMIT, pb.OrderSpecSide.SELL, self.params["qty"], option_price)
+                        print("placed order")
+
+
 
     def black_scholes(self, underlying_price, params):
         S = underlying_price["underlying"]
@@ -82,6 +103,7 @@ class BlackScholesBot(UTCBot):
 
         call_price = S * norm.cdf(d1) - K * exp(-r * T) * norm.cdf(d2)
         return call_price
+
 
 if __name__ == "__main__":
     start_bot(BlackScholesBot)
