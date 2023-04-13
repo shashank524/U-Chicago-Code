67f643ca41694ac444f2e3c5867ff402e652b2f2
shashank524 <saishashank524@gmail.com>
2023-04-13 14:24:36 -0400

    new updates

--- case2_draft.py
+++ case2_draft.py
@@ -4,41 +4,23 @@
 A Black-Scholes model implementation
 '''
 
-from utc_bot import UTCBot, start_bot
-import proto.utc_bot as pb
-import betterproto
 import asyncio
 import json
 import pandas as pd
-import numpy as np
+from utc_bot import UTCBot, start_bot
+import proto.utc_bot as pb
+import betterproto
+from math import sqrt, exp
 from scipy.stats import norm
-from scipy.optimize import brentq
-
-# Import additional libraries
-import os
 
 PARAM_FILE = "params.json"
 CSV_FILE = "training_pricepaths.csv"
 
-# Read the CSV file into a DataFrame
-price_data = pd.read_csv(CSV_FILE)
-
-# Black-Scholes formula
-def black_scholes_call(S, K, T, r, sigma):
-    d1 = (np.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * np.sqrt(T))
-    d2 = d1 - sigma * np.sqrt(T)
-    return S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
-
-def implied_volatility(S, K, T, r, C):
-    def objective_function(sigma):
-        return black_scholes_call(S, K, T, r, sigma) - C
-
-    return brentq(objective_function, 1e-6, 1e6)
-
-class OptionBot(UTCBot):
-    """
-    An example bot that reads from a file to set internal parameters during the round
-    """
+class BlackScholesBot(UTCBot):
+    def __init__(self):
+        super().__init__()
+        self.params = None
+        self.option_data = pd.read_csv(CSV_FILE)
 
     async def handle_round_started(self):
         await asyncio.sleep(0.1)
@@ -46,12 +28,11 @@ class OptionBot(UTCBot):
 
     async def handle_exchange_update(self, update: pb.FeedMessage):
         kind, _ = betterproto.which_one_of(update, "msg")
-        # Competition event messages
-        if kind == "generic_msg":
+        if kind == "market_snapshot_msg":
+            await self.handle_market_snapshot(update.market_snapshot_msg)
+        elif kind == "generic_msg":
             msg = update.generic_msg.message
             print(msg)
-
-        # Add your Black-Scholes model logic here
 
     async def handle_read_params(self):
         while True:
@@ -62,6 +43,32 @@ class OptionBot(UTCBot):
 
             await asyncio.sleep(1)
 
-if __name__ == "__main__":
-    start_bot(OptionBot)
+    async def handle_market_snapshot(self, snapshot: pb.MarketSnapshotMessage):
+        for asset_code, book in snapshot.books.items():
+            underlying_price = self.option_data.loc[self.option_data['underlying'] == asset_code].iloc[0]
+            option_price = self.black_scholes(underlying_price, self.params)
+            
+            if book.bids and book.asks:
+                bid_price = float(book.bids[0].px)
+                ask_price = float(book.asks[0].px)
+                
+                if option_price < bid_price:
+                    await self.place_order(asset_code, pb.OrderSpecType.LIMIT, pb.OrderSpecSide.BUY, self.params["qty"], option_price)
+                elif option_price > ask_price:
+                    await self.place_order(asset_code, pb.OrderSpecType.LIMIT, pb.OrderSpecSide.SELL, self.params["qty"], option_price)
 
+    def black_scholes(self, underlying_price, params):
+        S = underlying_price["underlying"]
+        K = params["K"]
+        T = params["T"]
+        r = params["r"]
+        sigma = params["sigma"]
+
+        d1 = (log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
+        d2 = d1 - sigma * sqrt(T)
+
+        call_price = S * norm.cdf(d1) - K * exp(-r * T) * norm.cdf(d2)
+        return call_price
+
+if __name__ == "__main__":
+    start_bot(BlackScholesBot)
