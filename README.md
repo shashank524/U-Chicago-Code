d538af11e4c479fb762e9e04fc2b35365ce9fb2c
shashank524 <saishashank524@gmail.com>
2023-04-13 20:55:03 -0400

    CASE 2 WORKS!!

---
+++ README.md
@@ -0,0 +1,3 @@
+python3 clients/case2_draft.py "PurdueUniversity" --key Spearow20 --host case2.uchicagotrading.com --port 9090
+
+
--- xchange-v1.0.2-115-g6b94+8ef0/clients/case2_draft.py
+++ xchange-v1.0.2-115-g6b94+8ef0/clients/case2_draft.py
@@ -21,11 +21,16 @@ PARAM_FILE = "params.json"
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 CSV_FILE = os.path.join(SCRIPT_DIR, "..", "data", "case2", "training_pricepaths.csv")
 
+
+def round_to_tick_size(price, tick_size=0.1):
+    return round(price / tick_size) * tick_size
+
 class BlackScholesBot(UTCBot):
     def __init__(self, username, key, host, port):
         super().__init__(username, key, host, port)
         self.params = None
         self.option_data = pd.read_csv(CSV_FILE, index_col=0)
+
 
 
     async def handle_round_started(self):
@@ -35,10 +40,14 @@ class BlackScholesBot(UTCBot):
     async def handle_exchange_update(self, update: pb.FeedMessage):
         kind, _ = betterproto.which_one_of(update, "msg")
         if kind == "market_snapshot_msg":
-            await self.handle_market_snapshot(update.market_snapshot_msg)
+            # Obtain the underlying price from self.option_data
+            underlying_price = self.option_data.loc[0, 'underlying']
+
+            await self.handle_market_snapshot(update.market_snapshot_msg, underlying_price)
         elif kind == "generic_msg":
             msg = update.generic_msg.message
             print(msg)
+
 
     async def handle_read_params(self):
         while True:
@@ -49,21 +58,19 @@ class BlackScholesBot(UTCBot):
 
             await asyncio.sleep(1)
 
-    async def handle_market_snapshot(self, snapshot: pb.MarketSnapshotMessage):
-        for asset_code, book in snapshot.books.items():
-            print("Asset Code:", asset_code)
 
-            # Extract the underlying price, strike price, and option type from the asset_code
-            match = re.match(r"(\d+(?:\.\d+)?)-(\d+(?:\.\d+)?)([CP])", asset_code)
+    async def handle_market_snapshot(self, snapshot: pb.MarketSnapshotMessage, underlying_price: float):
+        for asset_code, book in snapshot.books.items():
+            # Extract the strike price and option type from the asset_code
+            match = re.match(r"SPY(\d+(?:\.\d+)?)([CP])", asset_code)
 
             if match:
-                print("here")
-                underlying_px, strike_price, option_type = match.groups()
-                underlying_px = float(underlying_px)
+                strike_price, option_type = match.groups()
                 strike_price = float(strike_price)
 
                 # Get the closest underlying price row in the option data
-                underlying_rows = self.option_data.iloc[(self.option_data['underlying'] - underlying_px).abs().argsort()[:1]]
+                underlying_rows = self.option_data.iloc[(self.option_data['underlying'] - underlying_price).abs().argsort()[:1]]
+
                 underlying_price = underlying_rows.iloc[0]
 
                 # Calculate the option price using the Black-Scholes model
@@ -72,21 +79,25 @@ class BlackScholesBot(UTCBot):
                 option_price = self.black_scholes(underlying_price, bs_params)
 
                 if option_type == "C":
-                    option_price = underlying_price[f"call{strike_price}"]
+                    # option_price = underlying_price[f"call{strike_price}"]
+                    option_price = underlying_price[f"call{int(strike_price)}"]
                 elif option_type == "P":
-                    option_price = underlying_price[f"put{strike_price}"]
+                    option_price = underlying_price[f"put{int(strike_price)}"]
+                    # option_price = underlying_price[f"put{strike_price}"]
 
                 if book.bids and book.asks:
                     bid_price = float(book.bids[0].px)
                     ask_price = float(book.asks[0].px)
 
+                    rounded_option_price = round_to_tick_size(option_price)
+
                     if option_price < bid_price:
                         print("placing order")
-                        await self.place_order(asset_code, pb.OrderSpecType.LIMIT, pb.OrderSpecSide.BUY, self.params["qty"], option_price)
+                        await self.place_order(asset_code, pb.OrderSpecType.LIMIT, pb.OrderSpecSide.BID, self.params["qty"], rounded_option_price)
                         print("placed order")
                     elif option_price > ask_price:
                         print("placing order")
-                        await self.place_order(asset_code, pb.OrderSpecType.LIMIT, pb.OrderSpecSide.SELL, self.params["qty"], option_price)
+                        await self.place_order(asset_code, pb.OrderSpecType.LIMIT, pb.OrderSpecSide.ASK, self.params["qty"], rounded_option_price)
                         print("placed order")
 
 
